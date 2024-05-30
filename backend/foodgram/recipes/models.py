5f17a2520b6e2fe17fa214c218cc5031a4e463a4
SergeyVolkoff <vol7745@yandex.ru>
2024-05-30 09:19:29 +0300

    add model tag

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=100,
+        unique=True
+    )
+    slug = models.SlugField()
+    color = models.CharField(
+        verbose_name = 'Название',
+        max_length=100,
+    )
+
+    
