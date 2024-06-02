f42b22ba8cc67a39f58eb350e200855bf72df9b7
SergeyVolkoff <vol7745@yandex.ru>
2024-06-02 10:51:06 +0300

    add model recipe, ingr,tag

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from backend.foodgram.users.models import Users
+
 
 class Tag(models.Model):
     name = models.CharField(
@@ -13,4 +15,48 @@ class Tag(models.Model):
         max_length=100,
     )
 
-    
+
+class Ingredient(models.Model):
+    units_measure = models.CharField(
+        max_length=150,
+        verbose_name='Единицы измерения'
+    )
+
+
+class Recipe(models.Model):
+
+    author = models.ForeignKey(
+        Users,
+        on_delete=models.CASCADE,
+        verbose_name='Автор рецепта'
+    )
+    tag = models.ManyToManyField(
+        Tag,
+        verbose_name="Tags"
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингридиенты',
+        through='RecipeIngredient'
+    )
+    image = models.ImageField(
+        upload_to='recipes/images/'
+    )
+    text = models.TextField(
+        'Text',
+        help_text="Input text"
+    )
+    coocing_time = models.PositiveBigIntegerField(
+        verbose_name='Время приготовления',
+        help_text='Введите время приготовления в минутах'
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True,
+    )
+
+
+class RecipeIngredient(models.Model):
+    """For Recipe&Ingrredient"""
+    pass
