60515dcd53dc6cb3acc13e690e9c60aa0344a4c7
SergeyVolkoff <vol7745@yandex.ru>
2024-06-17 21:59:03 +0300

    bi local add wsgi and nginx. admin,api work in local with static

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -120,7 +120,8 @@ AUTH_USER_MODEL = 'users.Users'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend' 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
