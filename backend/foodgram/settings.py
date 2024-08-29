00af5ff5588edf4fd10261b52a4b34a5960ee4c1
SergeyVolkoff <vol7745@yandex.ru>
2024-08-29 21:43:38 +0300

    fix reviewer sett1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,14 +5,12 @@ import rest_framework.permissions
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECRET_KEY = 'django-insecure-1cv%#e9#-oi!y^ik9h)nl63n(m@p2yoifo9*&1=ym0ys)iys^$'
-SECRET_KEY = os.getenv('SECRET_KEY', )
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # ALLOWED_HOSTS = ['51.250.26.198', '127.0.0.1', 'localhost', 'foodgramic.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-
-DEBUG = os.getenv('DEBUG', )
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
